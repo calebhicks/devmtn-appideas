c3d4e6e530c068c57d335c6fa62f38408a5c9855
Caleb Hicks <calebhicks@gmail.com>
2014-05-27 18:19:32 -0600

    Initial Commit

---
+++ AppIdeas/AXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AXAppDelegate.h
+//  AppIdeas
+//
+//  Created by Caleb Hicks on 5/27/14.
+//  Copyright (c) 2014 Dev Mountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppIdeas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppIdeas
+//
+//  Created by Caleb Hicks on 5/27/14.
+//  Copyright (c) 2014 Dev Mountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AXAppDelegate class]));
+    }
+}
